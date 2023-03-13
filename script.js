6cd2e630555eab4105da578f744fba93f6e621aa
Joceeeee <100770558+Joceeeee@users.noreply.github.com>
2023-03-13 18:00:00 +0800

    added hover effect

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ function changeColour(colour) {
 
 square.addEventListener('click', (event) => changeColour('green'));
 
+/* create hover effect*/
+square.addEventListener('mouseover', (event) => changeColour('grey'));
+square.addEventListener('mouseout', (event) => changeColour('red'));
+
 function createBuzzwordPhrase() {
     /* See https://en.wikipedia.org/wiki/List_of_buzzwords */
     let buzz = ["Paradigm-changing", "Multi-tier", "10,000-foot", "Agile", "Customer", "Win-win"];
