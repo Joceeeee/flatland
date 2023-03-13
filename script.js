1ad178df2b1cb0bb5cc7b1e79ac607f60692f6a8
Joceeeee <100770558+Joceeeee@users.noreply.github.com>
2023-03-13 17:53:29 +0800

    added semicolon

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function changeColour(color) {
     square.background = colour;
 }
 
-square.addEventListener('click', (event) => changeColour('green'))
+square.addEventListener('click', (event) => changeColour('green'));
 
 function createBuzzwordPhrase() {
     /* See https://en.wikipedia.org/wiki/List_of_buzzwords */
